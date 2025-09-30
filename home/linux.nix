dd82802b6aba97ec0eb02cc219820f0ca8d59053
Andy Lee <andylizf@outlook.com>
2025-09-30 15:40:09 -0700

    Harden sysctl detection for inotify tuning

--- home/linux.nix
+++ home/linux.nix
@@ -3,7 +3,18 @@
   # Linux-specific tweaks can go here.
 
   home.activation.ensureInotify = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    if ! command -v sysctl >/dev/null 2>&1; then
+    SYSCTL_BIN=""
+    if command -v sysctl >/dev/null 2>&1; then
+      SYSCTL_BIN="$(command -v sysctl)"
+    elif [ -x /usr/sbin/sysctl ]; then
+      SYSCTL_BIN="/usr/sbin/sysctl"
+    elif [ -x /sbin/sysctl ]; then
+      SYSCTL_BIN="/sbin/sysctl"
+    elif [ -x /bin/sysctl ]; then
+      SYSCTL_BIN="/bin/sysctl"
+    fi
+
+    if [ -z "$SYSCTL_BIN" ]; then
       echo "[dotfiles] sysctl not available; skipping inotify tuning" >&2
     else
       tmpFile="$(mktemp)"
@@ -14,7 +25,7 @@ EOF
       if [ "$(id -u)" -eq 0 ]; then
         install -Dm644 "$tmpFile" /etc/sysctl.d/99-inotify.conf
         rm -f "$tmpFile"
-        sysctl -p /etc/sysctl.d/99-inotify.conf || true
+        "$SYSCTL_BIN" -p /etc/sysctl.d/99-inotify.conf || true
       else
         SUDO_BIN=""
         if command -v sudo >/dev/null 2>&1; then
@@ -28,11 +39,11 @@ EOF
         if [ -n "$SUDO_BIN" ]; then
           "$SUDO_BIN" install -Dm644 "$tmpFile" /etc/sysctl.d/99-inotify.conf
           rm -f "$tmpFile"
-          "$SUDO_BIN" sysctl -p /etc/sysctl.d/99-inotify.conf || true
+          "$SUDO_BIN" "$SYSCTL_BIN" -p /etc/sysctl.d/99-inotify.conf || true
         elif command -v doas >/dev/null 2>&1; then
           doas install -Dm644 "$tmpFile" /etc/sysctl.d/99-inotify.conf
           rm -f "$tmpFile"
-          doas sysctl -p /etc/sysctl.d/99-inotify.conf || true
+          doas "$SYSCTL_BIN" -p /etc/sysctl.d/99-inotify.conf || true
         else
           rm -f "$tmpFile"
           echo "[dotfiles] no sudo/doas; skipping inotify sysctl" >&2
