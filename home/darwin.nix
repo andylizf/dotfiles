55ae19fdad429fe1de9c43f5ef0c9fe7c372b4d3
Andy Lee <andylizf@outlook.com>
2026-04-21 19:02:03 +0800

    chore: upd keys and darwin-specific settings

--- home/darwin.nix
+++ home/darwin.nix
@@ -1,5 +1,10 @@
 { ... }:
 {
-  # macOS-specific tweaks can go here.
+  targets.darwin.defaults = {
+    com.apple.finder.ShowPathbar = true;
+    com.apple.dock.autohide = true;
+    com.apple.dock.tilesize = 54;
+    NSGlobalDomain.AppleInterfaceStyleSwitchesAutomatically = true;
+  };
 }
 
